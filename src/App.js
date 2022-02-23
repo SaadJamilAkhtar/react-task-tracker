c6cb7c0cf2f63a2f10bf0d61a3d87ecdf96b4b1b
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-02-24 04:03:12 +0500

    added json-server

--- src/App.js
+++ src/App.js
@@ -5,26 +5,7 @@ import AddTask from "./components/AddTask";
 
 
 function App() {
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            text: "Doctor's Appointment",
-            day: "Feb 5th at 2:30pm",
-            reminder: true
-        },
-        {
-            id: 2,
-            text: "Meeting at School",
-            day: "Feb 6th at 2:30pm",
-            reminder: true
-        },
-        {
-            id: 3,
-            text: "Food Shopping",
-            day: "Feb 5th at 4:30pm",
-            reminder: false
-        },
-    ]);
+    const [tasks, setTasks] = useState([]);
     const [showForm, setShowForm] = useState(false);
 
     // Delete Task

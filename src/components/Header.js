a9e306ecb677ec978a4db7fe9d444c67fceaa07e
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-02-24 17:35:13 +0500

    minor adjustments

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from "./Button"
+import {useLocation} from "react-router-dom";
+
 function Header(props) {
+    const location = useLocation();
     return (
         <header className={"header"}>
             <h1>{props.title}</h1>
-            <Button color={props.showAdd? 'green': 'red'} text={props.showAdd? "Add +": "Close"} onClick={props.onToggle} />
+            {location.pathname === '/' && (
+                <Button color={props.showAdd ? 'green' : 'red'} text={props.showAdd ? "Add +" : "Close"}
+                        onClick={props.onToggle}/>)}
         </header>
     );
 }

3e47f48adc9eeab7c5a06e573061b9d7d8134cd2
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-02-24 17:31:26 +0500

    ADDED LINK FROM REACT-ROUTER-DOM

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 function About(props) {
     return (
         <div>
             <h4>Version 1.0.0</h4>
-            <a href="/">Go Back</a>
+            <Link to="/">Go Back</Link>
         </div>
     );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 function Footer(props) {
     return (
         <footer>
             <p>Copyright &copy; 2022</p>
-            <a href="/about">About</a>
+            <Link to="/about">About</Link>
         </footer>
     );
 }
